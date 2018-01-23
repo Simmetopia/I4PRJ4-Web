8dff546c4f3f1803ddb51b8fb549128e0ffe678c
Simone Bundgaard-Pedersen <simonebundgaard-pedersen@Simones-MacBook-Pro.local>
2018-01-23 18:07:18 +0100

    si

--- src/app/highscore-page/highscore-page.component.ts
+++ src/app/highscore-page/highscore-page.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { IUser } from '../Data/user.interface';
-import { MatTableDataSource } from '@angular/material/table';
-import { MatPaginator } from '@angular/material';
+import { Component, OnInit, ViewChild } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { IUser } from '../Data/user.interface'
+import { MatTableDataSource } from '@angular/material/table'
+import { MatPaginator } from '@angular/material'
 @Component({
   selector: 'app-highscore-page',
   templateUrl: './highscore-page.component.html',
@@ -10,26 +10,24 @@ import { MatPaginator } from '@angular/material';
 })
 export class HighscorePageComponent implements OnInit {
   constructor(private http: HttpClient) {}
-  users: IUser[];
-  dataSource;
-  displayedColumns = ['position', 'name', 'weight', 'symbol'];
-  @ViewChild(MatPaginator) paginator: MatPaginator;
+  users: IUser[]
+  dataSource
+  displayedColumns = ['position', 'name', 'weight', 'symbol']
+  @ViewChild(MatPaginator) paginator: MatPaginator
   ngOnInit() {
-    this.http
-      .get<IUser[]>('http://kingsofdanger.herokuapp.com/api/users')
-      .subscribe(data => {
-        this.users = data.sort(this.CompareFunc);
-        this.dataSource = new MatTableDataSource<IUser>(this.users);
-        this.dataSource.paginator = this.paginator;
-      });
+    this.http.get<IUser[]>('api/users').subscribe(data => {
+      this.users = data.sort(this.CompareFunc)
+      this.dataSource = new MatTableDataSource<IUser>(this.users)
+      this.dataSource.paginator = this.paginator
+    })
   }
   CompareFunc(user1: IUser, user2: IUser) {
     if (user1.totalLevel > user2.totalLevel) {
-      return -1;
+      return -1
     }
     if (user1.totalLevel < user2.totalLevel) {
-      return 1;
+      return 1
     }
-    return 0;
+    return 0
   }
 }
