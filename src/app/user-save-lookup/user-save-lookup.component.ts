41c80ca5ec80250f5a9012db7ed159ba14ad54a6
Simone Bundgaard-Pedersen <simonebundgaard-pedersen@Simones-MacBook-Pro.local>
2018-01-23 20:51:30 +0100

    donger

--- src/app/user-save-lookup/user-save-lookup.component.ts
+++ src/app/user-save-lookup/user-save-lookup.component.ts
@@ -1,12 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { IUser, ISave } from '../Data/user.interface';
-import {
-  FormControl,
-  Validators,
-  FormGroup,
-  FormBuilder
-} from '@angular/forms';
+import { Component, OnInit } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { IUser, ISave } from '../Data/user.interface'
+import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms'
 
 @Component({
   selector: 'app-user-save-lookup',
@@ -14,29 +9,27 @@ import {
   styleUrls: ['./user-save-lookup.component.css']
 })
 export class UserSaveLookupComponent implements OnInit {
-  user: ISave;
-  username: string;
-  searchUser: FormGroup;
+  user: ISave
+  username: string
+  searchUser: FormGroup
   constructor(private http: HttpClient, private fb: FormBuilder) {}
   OnSubmit(form: FormGroup) {
-    this.user = undefined;
-    this.username = form.get('name').value;
-    const a = this.username.toLocaleLowerCase();
-    this.http
-      .get<ISave>('http://kingsofdanger.herokuapp.com/api/users/showuser/' + a)
-      .subscribe(
-        data => {
-          this.user = data;
-          this.user.PlayerItems.slice(1, 6);
-        },
-        err => {
-          alert('user not found');
-        }
-      );
+    this.user = undefined
+    this.username = form.get('name').value
+    const a = this.username.toLocaleLowerCase()
+    this.http.get<ISave>('api/users/showuser/' + a).subscribe(
+      data => {
+        this.user = data
+        this.user.PlayerItems.slice(1, 6)
+      },
+      err => {
+        alert('user not found')
+      }
+    )
   }
   ngOnInit() {
     this.searchUser = this.fb.group({
       name: 'simon'
-    });
+    })
   }
 }
