e9a783f2cd0df2a96f0bbeb953c905aa97334f1c
Simone Bundgaard-Pedersen <simonebundgaard-pedersen@Simones-MacBook-Pro.local>
2018-02-01 16:44:59 +0100

    large files server

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const app = express();
 const api = require('./server/routes/api');
 app.use(cors());
 // Parsers
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 // Angular DIST output folder
 app.use(express.static(path.join(__dirname, 'dist')));
